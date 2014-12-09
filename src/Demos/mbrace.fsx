5fd121c84a49b35746a7c3ba36b5ab502bbb85c0
npal <nick.palladinos@gmail.com>
2014-12-09 17:00:53 +0200

    Test

--- src/Demos/mbrace.fsx
+++ src/Demos/mbrace.fsx
@@ -10,7 +10,7 @@ open Nessos.MBrace.Azure.Runtime
 let config = 
     { Configuration.Default with
         StorageConnectionString = Helpers.azureStorageConn
-        ServiceBusConnectionString =  Helpers.serviceBusConn }
+        ServiceBusConnectionString = Helpers.serviceBusConn }
 
 
 let runtime = Runtime.GetHandle(config)
@@ -19,17 +19,37 @@ let runtime = Runtime.GetHandle(config)
 runtime.ShowWorkers()
 runtime.ShowLogs()
 
+
+
 runtime.Run(cloud { return "Hello world"})
 
 
 
 open Nessos.MBrace.Streams
+open Nessos.Streams
+
+let cloudFiles = CloudFile.Enumerate "shakespeare" |> runtime.Run
+
+
+
+#time
 
 let xs = 
-    [|1..10|]
-    |> CloudStream.ofArray
-    |> CloudStream.map (fun x -> x * x)
-    |> CloudStream.toArray
+    cloudFiles
+    |> CloudStream.ofCloudFiles CloudFile.ReadAllLines
+    |> CloudStream.collect Stream.ofArray
+    |> CloudStream.filter (fun x -> x.ToUpper().Contains("KING") )
+    |> CloudStream.toCloudArray
 
 
-runtime.Run xs
+let x = runtime.Run xs
+
+let cached = CloudStream.cache x |> runtime.Run
+
+let result = 
+    cached
+    |> CloudStream.ofCloudArray
+    |> CloudStream.filter (fun x -> x.ToUpper().Contains("KING") )
+    |> CloudStream.toCloudArray
+
+result |> runtime.Run
