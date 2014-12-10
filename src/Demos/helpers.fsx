719832c610cd5e3f791bb3aa57c61fe0bef2d4cd
krontogiannis <rodoconst@hotmail.com>
2014-12-10 15:28:34 +0200

    Updated dlls, added sample

--- src/Demos/helpers.fsx
+++ src/Demos/helpers.fsx
@@ -27,3 +27,38 @@ Runtime.Spawn(config, 4)
 
 runtime.ClientLogger.Attach(Common.ConsoleLogger())
 *)
+
+
+
+open System
+open System.IO
+open System.Text.RegularExpressions
+
+
+/// words ignored by wordcount
+let noiseWords = 
+    set [
+        "a"; "about"; "above"; "all"; "along"; "also"; "although"; "am"; "an"; "any"; "are"; "aren't"; "as"; "at";
+        "be"; "because"; "been"; "but"; "by"; "can"; "cannot"; "could"; "couldn't"; "did"; "didn't"; "do"; "does"; 
+        "doesn't"; "e.g."; "either"; "etc"; "etc."; "even"; "ever";"for"; "from"; "further"; "get"; "gets"; "got"; 
+        "had"; "hardly"; "has"; "hasn't"; "having"; "he"; "hence"; "her"; "here"; "hereby"; "herein"; "hereof"; 
+        "hereon"; "hereto"; "herewith"; "him"; "his"; "how"; "however"; "I"; "i.e."; "if"; "into"; "it"; "it's"; "its";
+        "me"; "more"; "most"; "mr"; "my"; "near"; "nor"; "now"; "of"; "onto"; "other"; "our"; "out"; "over"; "really"; 
+        "said"; "same"; "she"; "should"; "shouldn't"; "since"; "so"; "some"; "such"; "than"; "that"; "the"; "their"; 
+        "them"; "then"; "there"; "thereby"; "therefore"; "therefrom"; "therein"; "thereof"; "thereon"; "thereto"; 
+        "therewith"; "these"; "they"; "this"; "those"; "through"; "thus"; "to"; "too"; "under"; "until"; "unto"; "upon";
+        "us"; "very"; "viz"; "was"; "wasn't"; "we"; "were"; "what"; "when"; "where"; "whereby"; "wherein"; "whether";
+        "which"; "while"; "who"; "whom"; "whose"; "why"; "with"; "without"; "would"; "you"; "your" ; "have"; "thou"; "will"; 
+        "shall"
+    ]
+
+// Splits a string into words
+let splitWords =
+    let regex = new Regex(@"[\W]+", RegexOptions.Compiled)
+    fun text -> regex.Split(text)
+
+let wordTransform (word : string) = word.Trim().ToLower()
+
+let wordFilter (word : string) = word.Length > 3 && not <| noiseWords.Contains(word)
+
+
--- src/Demos/mbrace.fsx
+++ src/Demos/mbrace.fsx
@@ -23,6 +23,7 @@ runtime.Run(cloud { return "Hello world"})
 
 
 
+open Nessos.Streams
 open Nessos.MBrace.Streams
 
 let xs = 
@@ -33,3 +34,43 @@ let xs =
 
 
 runtime.Run xs
+
+let ps1 = runtime.CreateProcess(CloudFile.Enumerate "wikipedia", name = "enumeratefiles")
+let files = ps1.AwaitResult()
+
+let getTop files count =
+    files
+    |> CloudStream.ofCloudFiles CloudFile.ReadAllText
+    |> CloudStream.collect (fun text -> Helpers.splitWords text |> Stream.ofArray |> Stream.map Helpers.wordTransform)
+    |> CloudStream.filter Helpers.wordFilter
+    |> CloudStream.countBy id
+    |> CloudStream.sortBy (fun (_,c) -> -c) count
+    |> CloudStream.toArray
+
+let ps = runtime.CreateProcess(getTop files 20, name = "getTop with cloudfiles")
+let r = ps.AwaitResult()
+
+
+let mkCloudArray files =
+    cloud {
+        let! cloudarray =
+            files
+            |> CloudStream.ofCloudFiles CloudFile.ReadAllText
+            |> CloudStream.collect (fun text -> Helpers.splitWords text |> Stream.ofArray |> Stream.map Helpers.wordTransform)
+            |> CloudStream.filter Helpers.wordFilter
+            |> CloudStream.toCloudArray
+        return! CloudStream.cache cloudarray
+    }
+
+let getTop' cloudarray count =
+    cloudarray
+    |> CloudStream.ofCloudArray
+    |> CloudStream.countBy id
+    |> CloudStream.sortBy (fun (_,c) -> -c) count
+    |> CloudStream.toArray
+
+let caProcess = runtime.CreateProcess(mkCloudArray files, name = "create cloudarray")
+let ca = caProcess.AwaitResult()
+
+let ps' = runtime.CreateProcess(getTop' ca 20, name = "getTop with cloudarray")
+let r' = ps'.AwaitResult()
